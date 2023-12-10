ae64250aa877072787f767bb46b923de8ebdfd73
jaegon-kim <jaegon.kim@gmail.com>
2023-12-10 22:10:04 +0900

    parity O(n)

--- src/epi/ch4primitive/p4_1_parity.cpp
+++ src/epi/ch4primitive/p4_1_parity.cpp
@@ -1,7 +1,7 @@
 
 #include <iostream>
 
-int count_bit(long n) {
+int count_bit(unsigned int n) {
     long num_bit = 0;
     while(n) {
         num_bit += (n & 1);
@@ -10,14 +10,49 @@ int count_bit(long n) {
     return num_bit;
 }
 
+void test_xor() {
+    std::cout << "1 ^ 1 = " << (1 ^ 1) << std::endl;
+    std::cout << "1 ^ 0 = " << (1 ^ 0) << std::endl;
+    std::cout << "0 ^ 1 = " << (0 ^ 1) << std::endl;
+    std::cout << "0 ^ 0 = " << (0 ^ 0) << std::endl;
+}
+
+int parity_o_n(unsigned long long n) {
+
+    unsigned long long parity = 0;
+    while (n) {
+        parity ^= (n & 1);
+            // 1 ^ Odd(1) = Even(0)
+            // 0 ^ Odd(1) = Odd(1)
+            // 1 ^ Even(0) = Odd(1)
+            // 0 ^ Even(0) = Even(0)
+
+        n >>= 1;
+    } 
+    return (int) parity;
+}
+
 void test_count_bit() {
-    std::cout << count_bit(3) << std::endl;
-    std::cout << count_bit(0b010101) << std::endl; // binary literal
-    std::cout << count_bit(0b111100001111) << std::endl;
+    std::cout << count_bit(3);
+    std::cout << "(" << parity_o_n(3) << ")" << std::endl;
+
+    std::cout << count_bit(0b010101); // binary literal
+    std::cout << "(" << parity_o_n(0b010101) << ")" << std::endl; // binary literal
+
+    std::cout << count_bit(0b111100001111);
+    std::cout << "(" << parity_o_n(0b111100001111) << ")" << std::endl;
+
+    std::cout << count_bit(0b111111111110);
+    std::cout << "(" << parity_o_n(0b111111111110) << ")" << std::endl;
+
+    std::cout << count_bit(0b111111111111);
+    std::cout << "(" << parity_o_n(0b111111111111) << ")" << std::endl;
 
 }
+
 
 void test_p4_1_parity() {
     std::cout << "test parity" << std::endl;
     test_count_bit();
+    test_xor();
 }
