122875c561337a71177b83910d7f4dfebdd3d10b
jaegon-kim <jaegon.kim@gmail.com>
2023-12-10 23:33:04 +0900

    EPI problem 4-1 parity calculation

--- src/epi/ch4primitive/p4_1_parity.cpp
+++ src/epi/ch4primitive/p4_1_parity.cpp
@@ -45,26 +45,86 @@ int parity_o_k(unsigned long long n) {
     return (int) parity;
 }
 
+int parity16_o_k(unsigned short n) {
+
+    unsigned short parity = 0;
+    while (n) {
+        parity ^= 1;
+        n &= (n - 1); // It removes last '1' bit
+    } 
+    return (int) parity;
+}
+
+int map[65536];
+
+void init_map() {
+    for (unsigned int i = 0; i < 65536; i++) {
+        map[i] = parity16_o_k((short)i);
+    }
+}
+
+// EPI p39
+int parity_o_4(unsigned long long n) {
+
+    int parity = map[ n & 0xffffUL];
+    parity ^= map [(n & 0xffff0000UL) >> 16];
+    parity ^= map [(n & 0xffff00000000UL) >> 32];
+    parity ^= map [(n & 0xffff00000000UL) >> 48];
+
+    return parity;
+}
+
+// EPI p40
+int parity_o_log_n(unsigned long long n) {
+    unsigned long long parity = n;
+    parity = (parity & 0xffffffffUL) ^ (parity >> 32);
+    parity = (parity & 0xffffUL) ^ (parity >> 16);
+    parity = (parity & 0xffUL) ^ (parity >> 8);
+    parity = (parity & 0xfUL) ^ (parity >> 4);
+    parity = (parity & 0x3UL) ^ (parity >> 2);
+    parity = (parity & 0x1UL) ^ (parity >> 1);
+    return parity;
+}
+
+
 void test_count_bit() {
+
+    init_map();
+
     std::cout << count_bit(3);
     std::cout << "(" << parity_o_n(3) << "), ";
-    std::cout << "(" << parity_o_k(3) << ")" << std::endl;
+    std::cout << "(" << parity_o_k(3) << "), ";
+    std::cout << "(" << parity_o_4(3) << "), ";
+    std::cout << "(" << parity_o_log_n(3) << "), ";
+    std::cout << std::endl;
 
     std::cout << count_bit(0b010101); // binary literal
     std::cout << "(" << parity_o_n(0b010101) << "), "; // binary literal
-    std::cout << "(" << parity_o_k(0b010101) << ")" << std::endl; // binary literal
+    std::cout << "(" << parity_o_k(0b010101) << "), "; // binary literal
+    std::cout << "(" << parity_o_4(0b010101) << "), ";
+    std::cout << "(" << parity_o_log_n(0b010101) << "), ";
+    std::cout << std::endl;
 
     std::cout << count_bit(0b111100001111);
     std::cout << "(" << parity_o_n(0b111100001111) << "), ";
-    std::cout << "(" << parity_o_k(0b111100001111) << ")" << std::endl;
+    std::cout << "(" << parity_o_k(0b111100001111) << "), ";
+    std::cout << "(" << parity_o_4(0b111100001111) << "), ";
+    std::cout << "(" << parity_o_log_n(0b111100001111) << "), ";
+    std::cout << std::endl;
 
     std::cout << count_bit(0b111111111110);
     std::cout << "(" << parity_o_n(0b111111111110) << "), ";
-    std::cout << "(" << parity_o_k(0b111111111110) << ")" << std::endl;
+    std::cout << "(" << parity_o_k(0b111111111110) << "), ";
+    std::cout << "(" << parity_o_4(0b111111111110) << "), ";
+    std::cout << "(" << parity_o_log_n(0b111111111110) << "), ";
+    std::cout << std::endl;
 
     std::cout << count_bit(0b111111111111);
     std::cout << "(" << parity_o_n(0b111111111111) << "), ";
-    std::cout << "(" << parity_o_k(0b111111111111) << ")" << std::endl;
+    std::cout << "(" << parity_o_k(0b111111111111) << "), ";
+    std::cout << "(" << parity_o_4(0b111111111111) << "), " ;
+    std::cout << "(" << parity_o_log_n(0b111111111111) << ")," ;
+    std::cout << std::endl;
 
 }
 
