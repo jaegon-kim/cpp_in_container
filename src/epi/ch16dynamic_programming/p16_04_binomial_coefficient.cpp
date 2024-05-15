d940f261832f13503d16a33af86fc53ba968b07a
jaegon-kim <jaegon.kim@gmail.com>
2024-05-15 23:51:59 +0000

    p16-04 binomail coefficient - adding pascal's row for calculating the number of subent (n:total number of element, k: the number of subset)

--- src/epi/ch16dynamic_programming/p16_04_binomial_coefficient.cpp
+++ src/epi/ch16dynamic_programming/p16_04_binomial_coefficient.cpp
@@ -7,7 +7,7 @@ namespace p16_04 {
 
     int subset(int c, int k, vector<int> & v, vector<int> & subset_v) {
         if (!k) {
-            dump_vec(subset_v, true);
+            //dump_vec(subset_v, true);
             return 1;
         }
 
@@ -21,15 +21,37 @@ namespace p16_04 {
         return cnt;
     }
 
+    size_t pascal_row(size_t n, size_t k) {
+        if (n == k) {
+            return 1;
+        }
+
+        if (k == 1) {
+            return n;
+        }
+
+        return pascal_row(n - 1, k) + pascal_row(n - 1, k - 1);
+    }
+
     void test(int n, int k) {
         vector<int> v;
         for (int i = 1; i <= n; i++) {
             v.push_back(i);
         }
         vector<int> subset_v;
-        int r = subset(-1, k, v, subset_v);
-        cout << "n: " << n << ", k: " << k << ", # of subset: " <<  r << endl;
+
+        int r_s = subset(-1, k, v, subset_v);
+        size_t r_p = pascal_row(n, k);
+
+        cout << "n: " << n << ", k: " << k << ", # of subset: " <<  r_s << ", pascal's row: " << r_p << endl;
     }
+
+    void test_pascal(int n, int k) {
+        size_t r_p = pascal_row(n, k);
+        cout << "n: " << n << ", k: " << k << ", pascal's row: " << r_p << endl;
+    }
+
+
 }
 
 void test_p16_04_binomial_coefficient() {
@@ -38,4 +60,14 @@ void test_p16_04_binomial_coefficient() {
     p16_04::test(4, 3);
     p16_04::test(5, 2);
     p16_04::test(5, 3);
+    
+    p16_04::test(100, 3);
+    p16_04::test(200, 4);
+    
+    // recursion of subset takes too long time
+    //p16_04::test(300, 5);
+
+    // only pascal's row for binomial coefficent can be used for calculating the number of subset
+    p16_04::test_pascal(300, 5);
+
 }
