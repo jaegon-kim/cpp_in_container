6492c5e74d897ba17900b65b254218e56041463f
jaegon-kim <jaegon.kim@gmail.com>
2025-02-20 12:43:31 +0900

    describing problem for space ship

--- src/algorithm/a04_greedy/a04_01_spaceship.cpp
+++ src/algorithm/a04_greedy/a04_01_spaceship.cpp
@@ -4,6 +4,32 @@
 #include <utility/dump.hpp>
 using namespace std;
 
+/*
+0 ~ 124 사이의 높낮이 차이가 있는 1000x1000의 2차원 맵이 주어집니다. 이 맵에 1만대의 Spaceship을 착륙시키려고 합니다. 
+ Spaceship은 가로폭과 세로폭이 있는 사각형 모양이고, 가로폭과 세로폭은 2 ~ 5 사이의 크기를 갖습니다. 
+ Spaceship의 4개의 꼭지점의 높이차가 6을 초과하는 지역에는  착륙시킬 수 없습니다.
+ 또, Spaceship은 90도 회전할 수 있습니다. 
+
+가장 높은 스코어를 획득할 수 있도록 최대한 많은 Spaceship을 착륙시킬 수 있는 process 함수를 작성해 주세요.
+c언어로 작성을 부탁드립니다.
+
+ init과 process 함수가 있고, init에서 1000x1000의 2차원 맵에 랜덤하게 높이를 지정하고 Spaceship의 크기를 랜덤하게 지정합니다.
+ process에서 어떤 Spaceship을 어디에, 어느 방향으로 착륙시킬지를 결정하면 됩니다. 
+ 기본적으로 많은 Spaceship을 착륙시킬 수록 고득점이며, Spaceship의 가로폭이 w, 세로폭이 h라고 했을 때 1대당 구체적인 스코어 획득량은 w * h * MIN(w,h) 입니다.
+
+* Related Site
+(Stack Overflow)
+https://stackoverflow.com/questions/7342389/generalizing-the-algorithm-for-domino-tiling
+(검색어)
+tiling a rectangular grid biparite graph
+
+* Polyomino Tiler
+(Web Test) https://gfredericks.com/things/polyominoes
+*/
+
+
+
+
 namespace a04_01 {
     const static int N = 1000;
     const static int NUM_SPACESHIPS = 10000;
